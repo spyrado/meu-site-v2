820ade657cb96624d60d24bd5fc7e65764aaf562
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-03-16 10:47:10 -0300

    [GULP] configurando parar executar o sass watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
+'use strict';
+
 const gulp = require('gulp')
-  ,server = require('gulp-server-livereload');
- 
+  ,server = require('gulp-server-livereload')
+  ,sass = require('gulp-sass');
+
+  sass.compiler = require('node-sass');
+
+  
+  gulp.task('sass', function () {
+    return gulp.src('./src/scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./dist/css'));
+  });
+   
+  gulp.task('sass:watch', function () {
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+  });
+
+// INICIA UM SERVIDOR COM LIVERELOAD
 gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(server({
