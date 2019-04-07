62a2c4990440191b8bbf40c844a6adf484c96c94
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-04-07 11:57:06 -0300

    limpando e reformulando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,37 @@
 
 const gulp = require('gulp')
   ,server = require('gulp-server-livereload')
-  ,sass = require('gulp-sass');
-
+  ,sass = require('gulp-sass')
+  ,htmlmin = require('gulp-htmlmin')
+  ,imagemin = require('gulp-imagemin')
+  ,cleanCSS = require('gulp-clean-css');
+  
   sass.compiler = require('node-sass');
 
+  gulp.task('default', gulp.series(watch, gulp.parallel(minificaCss, minificaHtml),
+
+    function (done) { done(); }    
+));
+
+  function minificaCss(){
+    return gulp.src('dist/css/estilos.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('dist/css'));
+  }
+
+  function minificaHtml(){
+    return gulp.src('src/html/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('./'));
+  }
   
+
+  gulp.task('minify-img', () => {
+    return gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+  });
+
   gulp.task('sass', function () {
     return gulp.src('src/scss/estilos.scss')
       .pipe(sass().on('error', sass.logError))
--- src/js/app/services/APIWhatsApp.js
+++ src/js/app/services/APIWhatsApp.js
@@ -1,7 +1,6 @@
 export class APIWhatsApp{
 
   static enviaMensagem(mensagem){
-    alert('SYSTEMA DE MODULOS');
     window.open(`https://api.whatsapp.com/send?phone=5511961435069&text=${mensagem}`, '_blank');
   }
 }
