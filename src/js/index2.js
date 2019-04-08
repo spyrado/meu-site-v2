c3a633ef588baffb7e3e4f36a2d5eb7748d6bc71
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-04-07 21:50:51 -0300

    Refinando o layout

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,6 +1,30 @@
-document.querySelector('.envia-msg-whatsapp')
-  .addEventListener('submit', function(event){
-     event.preventDefault();
-     let mensagem = document.querySelector('#msg-whatsapp').value;
-     window.open(`https://api.whatsapp.com/send?phone=5511961435069&text=${mensagem}`, '_blank');
-  });
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function() {
+   'use strict';
+   window.addEventListener('load', function() {
+     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+     var forms = document.getElementsByClassName('needs-validation');
+     // Loop over them and prevent submission
+     var validation = Array.prototype.filter.call(forms, function(form) {
+       form.addEventListener('submit', function(event) {
+         if (form.checkValidity() === false) {
+           event.preventDefault();
+           event.stopPropagation();
+         }else{
+            enviaMensagemWhatsApp();
+         }
+         form.classList.add('was-validated');
+         
+       }, false);
+     });
+   }, false);
+ })();
+
+function enviaMensagemWhatsApp(){
+      let self = document.querySelector('.envia-msg-whatsapp')
+      let nome = self.nome.value;
+      let assunto = self.assunto.value;
+      let mensagem = self.mensagem.value;
+      window.open(`https://api.whatsapp.com/send?phone=5511961435069&text=Olá, prazer meu nome é *${nome}*
+      ${mensagem}`, '_blank');
+}
